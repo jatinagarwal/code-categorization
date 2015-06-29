3c534dee6ec4a89369f7ea6593c1525b0ec398ec
jatina <jatin.agarwal@imaginea.com>
2015-06-29 10:58:15 +0530

    implemented with idf and tf

--- code/lsaApp/src/main/scala/lsaApp.scala
+++ code/lsaApp/src/main/scala/lsaApp.scala
@@ -3,6 +3,9 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 import scala.util.matching.Regex
+import scala.collection.mutable.ListBuffer
+import scala.collection.immutable.Map
+
 
 object LsaApp {
   def main(args: Array[String]) {
@@ -10,30 +13,79 @@ object LsaApp {
   		"boolean", "do", "if", "private", "this", "break", "double", "implements", "protected", "throw", "byte", "else",
   		"import", "public", "throws", "case", "enum", "instanceof", "return", "transient", "catch", "extends", "int",
   		"short", "try", "char", "final", "interface", "static", "void", "class", "finally", "long", "strictfp", "volatile",
-  		"const", "float", "native", "super", "while", "true", "false", "null")
+  		"const", "float", "native", "super", "while", "true", "false", "null", "String", "java", "util", "ArrayList", "println",
+      "Arrays", "System", "File", "main")
 
-    val dataFiles = "sampleJava" // Should be some file on your system
+    val numTerms = 20
+    val dataFiles = "smallSampleJava" // Should be some file on your system
+
     val conf = new SparkConf().setAppName("Lsa Application")
     val sc = new SparkContext(conf)
+
     val codeData = sc.wholeTextFiles(dataFiles).cache()
+
     codeData.persist()
+
     val codeDataWithOutComments = codeData.mapValues{inp => 
     	val regex = """(/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/)|(//.*)""".r
     	val specialChar = """[^a-zA-Z\n\s\#]""".r
     	val commentsRemoved = regex.replaceAllIn(inp,"")
     	specialChar.replaceAllIn(commentsRemoved, " ")
     }	
+
+    val reserveWords = sc.broadcast(keywords)
+
     val words = codeDataWithOutComments.mapValues{inp =>
-    	inp.split("//s+")
-	}
+      val listOfWords = inp.split("\\s+").toList
+      val allReserveWords = reserveWords.value
+      val removeReserveWords =  new ListBuffer[String]()
+      for(word <- listOfWords if !allReserveWords.contains(word)) removeReserveWords+=word
+      val bagOfWords = removeReserveWords.toList.sorted
+      bagOfWords
+	  }
+
+    words.persist()
+
+    val documentFrequencies = words.flatMapValues{inp =>
+      inp.groupBy(x => x).mapValues(_.size)  
+    }.values.mapValues{ inp => inp/inp}.reduceByKey((x,y) => x+y).sortByKey()
+
+    val docFreqs = documentFrequencies.collect().sortBy(- _._2).take(numTerms)
+    val numDocs = words.count()
+
+    val idfs = inverseDocumentFrequencies(docFreqs, numDocs)
+
+    val vocabulary = documentFrequencies.keys
+
+    val termDocumentFrequencies = words.mapValues{inp =>
+      inp.groupBy(x => x).mapValues(_.size)
+    } 
+
     // val count : Int = codeData.collect().count()
     // val numAs = logData.filter(line => line.contains("a")).count()
     // val numBs = logData.filter(line => line.contains("b")).count()
     // println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
     // println("count of RDD: "+count)
-    codeData.take(2).foreach(println)
-    codeDataWithOutComments.take(2).foreach(println)
-    words.first()
+    // codeData.take(2).foreach(println)
+    // codeDataWithOutComments.take(2).foreach(println)
+    println("****************************************Number of documents *********************"+numDocs)
+    words.take(2).foreach(println)
+
+    println("**************************************************Number of documents*********************"+termDocumentFrequencies.count())
+    termDocumentFrequencies.values.foreach(println)
+
+    vocabulary.foreach(println)    
+
+    println("**************************************************Total terms *********************"+ documentFrequencies.count())
+    documentFrequencies.foreach(println)
+
+    docFreqs.foreach(println)
+    idfs.foreach(println) 
+  }
+
+  def inverseDocumentFrequencies(docFreqs: Array[(String, Int)], numDocs: Long)
+    : Map[String, Double] = {
+    docFreqs.map{ case (term, count) => (term, math.log(numDocs.toDouble / count))}.toMap
   }
 
   // def removeComments(inp: String): 
